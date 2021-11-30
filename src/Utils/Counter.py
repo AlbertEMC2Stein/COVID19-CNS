c86984c0968952b33501f8ee7a56437936288a03
prokosch <prokosch@rhrk.uni-kl.de>
2021-11-30 10:34:15 +0100

    Removed redundencies

--- src/Utils/Counter.py
+++ src/Utils/Counter.py
@@ -1,23 +1,21 @@
+import numpy as np
+
+
 class Counter:
     def __init__(self, start):
         self.n = start
+        self.history = np.array([start])
 
     def get_count(self):
         return self.n
 
-    def increment(self, k=1, return_when='after'):
-        self.n += k
+    def step(self, mode, k=1, return_when='after'):
+        if mode not in ["inc", "dec"]:
+            raise ValueError(str(mode) + " is not a valid value. Try 'inc' or 'dec'.")
 
-        if return_when == 'after':
-            return self.n
-        elif return_when == 'before':
-            return self.n - k
-        else:
-            raise ValueError(str(return_when) + " is not a valid value. Try 'after' or 'before'.")
-
-    def decrement(self, k=1, return_when='after'):
         old = self.n
-        self.n = max(0, self.n - k)
+        self.n = self.n + k if mode == "inc" else max(0, self.n - k)
+        self.history = np.append(self.history, self.n)
 
         if return_when == 'after':
             return self.n
