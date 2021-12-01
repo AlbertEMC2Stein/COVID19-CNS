f3935f791d8d70b532df9b7dcd39781020bf1ba8
prokosch <prokosch@rhrk.uni-kl.de>
2021-12-02 00:29:15 +0100

    Optimized code
    Loading populations is now much faster

--- src/Network/Population.py
+++ src/Network/Population.py
@@ -1,55 +1,68 @@
+import csv
+import json
 import numpy as np
 from os.path import sep
-import csv
-from Individual import Individual
-from Household import Household
+from src.Utils.ProgressBar import ProgressBar
+from .Member import Member
+from .Household import Household
 
 
 class Population:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.members = np.array([])
         self.households = {}
 
+    def __str__(self):
+        result = "Population: " + self.name + "\n"
+        for household in self.households.values():
+            result += str(household) + "\n"
 
-    def add_member(self, member, from_csv = False):
-        person = member
-        if not type(person) == Individual:
-            person = Individual(person)
+        return result[:-1]
+
+    def add_member(self, member: Member):
         try:
-            household_id = person.properties["household"]
-        except:
-            if from_csv:
-                raise KeyError("The csv-file is expected to have a \
-                               'household' column.")
-            else:
-                raise KeyError("Every member or person is expected to have a \
-                               'household' key.")
+            household_id = member.properties["household"]
+        except KeyError:
+            raise KeyError("Every individual is expected to have a 'household' property.")
+
         if household_id in self.households.keys():
-            self.households[household_id].add_member(person)
+            self.households[household_id].add_member(member)
         else:
             household = Household(household_id)
             self.households[household_id] = household
-            self.households[household_id].add_member(person)
-        self.members = np.append(self.members, person)
+            self.households[household_id].add_member(member)
 
+        if type(self.members) == np.ndarray:
+            self.members = np.append(self.members, member)
+        else:
+            self.members += [member]
 
-    def load_from_csv(self, csv_file):
-        with open(csv_file, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                person = Individual(row)
-                self.add_member(person, from_csv = True)
+    @staticmethod
+    def load_from_csv(file_name: str, path: str = "Populations" + sep):
+        p = Population(file_name[:-4])
+        p.members = []
 
+        with open(path + file_name, newline='') as f:
+            progress = ProgressBar(1, 1, sum(1 for _ in f) - 1)
 
-    def save_as_csv(self, path="out" + sep + "Simulated" + sep):
+        with open(path + file_name, newline='') as f:
+            for m_dict in csv.DictReader(f):
+                progress.update(1)
+                p.add_member(Member(m_dict))
+
+        p.members = np.array(p.members)
+        return p
+
+    def save_as_json(self, path: str = ".." + sep + "out" + sep + "Simulated" + sep):
         if len(self.members) == 0:
             raise ValueError("Population can't be empty.")
 
         with open(path + self.name + ".csv", 'w') as f:
             headers = self.members[0].properties.keys()
-            rows = np.array([list(member.properties.values) for member in self.members])
+            rows = np.array([list(member.properties.values()) for member in self.members], dtype=int)
 
             f.write(','.join(headers) + '\n')
             np.savetxt(f, rows, fmt='%d', delimiter=',')
+
 
