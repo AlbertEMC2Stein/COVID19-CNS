759a5bdeeaa7f03a3c260c2dd3822156ecd8dee8
prokosch <prokosch@rhrk.uni-kl.de>
2021-12-02 00:31:19 +0100

    First test of functionality

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from src.Network.Population import Population
+import numpy as np
+
 if __name__ == "__main__":
-    print("Hello world!")
+    p = Population.load_from_csv("DE_03_KLLand.csv")
+    mask = np.array([bool(int(m.properties['gender'])) for m in p.members])
+
+    print(*p.members[mask])
+
