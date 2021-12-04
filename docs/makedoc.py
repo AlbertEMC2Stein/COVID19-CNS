067c512e595592c3f7f9277bd13f4500028efc34
prokosch <prokosch@rhrk.uni-kl.de>
2021-12-04 17:37:04 +0100

    Docs are now put directly into the docs folder

--- docs/makedoc.py
+++ docs/makedoc.py
@@ -1,6 +1,11 @@
 import os
-
+from os.path import sep
 
 if __name__ == "__main__":
-    os.system("pdoc --html --force --config latex_math=True --output-dir . ../src")
+    os.system("pdoc --html --config latex_math=True --output-dir . .." + sep + "src")
+
+    for file in os.listdir("src"):
+        os.rename(os.getcwd() + sep + "src" + sep + file, os.getcwd() + sep + file)
+
+    os.rmdir("src")
 
