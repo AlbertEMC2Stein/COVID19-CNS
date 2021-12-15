211a7e3674906a061b6d0495598f8d886b1b4d37
prokosch <prokosch@rhrk.uni-kl.de>
2021-12-15 12:13:18 +0100

    Added TODO

--- docs/makedocs.py
+++ docs/makedocs.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     os.system("pdoc --html --config latex_math=True --output-dir . .." + sep + "src")
 
     assert(os.getcwd().split(sep)[-1] == "docs")
-    # TODO always navigate in docs no matter the cwd and do CI 
+    # TODO always navigate in docs no matter the cwd and do CI
 
     for file in os.listdir("src"):
         os.rename(os.getcwd() + sep + "src" + sep + file, os.getcwd() + sep + file)
