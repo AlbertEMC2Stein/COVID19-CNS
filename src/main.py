1d2689a09357af4bfddfdbb705436fc26dc47a17
prokosch <prokosch@rhrk.uni-kl.de>
2022-01-30 22:10:24 +0100

    Imporved heuristics

--- src/main.py
+++ src/main.py
@@ -8,32 +8,59 @@ import threading
 
 if __name__ == "__main__":
     def basic_infection_heuristic(mem_props):
-        return 1 - 1 / (0.001 * float(mem_props["age"]) + 1)
+        age = mem_props["age"]
+        if 0 <= age <= 4:
+            return 0.0330660325674
+        elif 5 <= age <= 14:
+            return 0.148281618844
+        elif 15 <= age <= 34:
+            return 0.304042732216
+        elif 35 <= age <= 59:
+            return 0.359434902279
+        elif 60 <= age <= 79:
+            return 0.109905306538
+        else:
+            return 0.0452694075548
 
     def basic_mortality_heuristic(mem_props):
-        return 1/10 * (float(mem_props["age"]) / 200) ** 5
+        age = mem_props["age"]
+        if 0 <= age <= 4:
+            return 0.000059062155147
+        elif 5 <= age <= 14:
+            return 0.0000124773674418
+        elif 15 <= age <= 34:
+            return 0.000111900272854
+        elif 35 <= age <= 59:
+            return 0.00176127729351
+        elif 60 <= age <= 79:
+            return 0.0338030065822
+        else:
+            return 0.170387357522
 
     simulation_settings = {
         "population_file": "DE_03_KLLand.csv",
         "infection_probability_heuristic": basic_infection_heuristic,
         "mortality_probability_heuristic": basic_mortality_heuristic,
-        "number_of_initially_infected": 100,
+        "number_of_initially_infected": 5,
         "number_of_initially_recovered": 0,
         "number_of_initially_vaccinated": 0,
         "inner_reproduction_number": 1,
-        "outer_reproduction_number": 2,
+        "outer_reproduction_number": 3,
         "override_newest": True,
         "incubation_time": 2,
         "infection_time": 14,
         "recovered_immunity_time": 90,
-        "vaccine_available_as_of": 0,
+        "vaccine_available_as_of": 365,
         "vaccination_takes_effect_time": 14,
-        "vaccinations_per_day": 100,
+        "vaccinations_per_day": 720,
         "vaccination_immunity_time": 90,
         "waiting_time_vaccination_until_new_vaccination": 90,
         "waiting_time_recovered_until_vaccination": 90,
+        "tests_per_day": 1000,
+        "test_vaccinated": True,  # FIXME make vaccinated infectable
+        "quarantine_duration": 10,
         "maximal_simulation_time_interval": 2*365,
-        "start_lockdown_at": 250,
+        "start_lockdown_at": 150,
         "end_lockdown_at": 50
     }
 
@@ -54,4 +81,6 @@ if __name__ == "__main__":
     Scenarios.single_simulation(simulation_settings)
     # Scenarios.mitigation_interval(simulation_settings, (1.5, 3), 16, 1)
 
-    PostProcessing.infection_graph("../out/DE_03_KLLand/0002")
+    folder = "../out/DE_03_KLLand/0004"
+    #PostProcessing.infection_graph(folder)
+    #PostProcessing.progression_plots(folder)
