4fa1d4f69451cc0575051adbc1637ed385edf713
prokosch <prokosch@rhrk.uni-kl.de>
2022-01-30 23:51:33 +0100

    Changed up heuristics

--- src/main.py
+++ src/main.py
@@ -7,40 +7,40 @@ import numpy as np
 import threading
 
 if __name__ == "__main__":
-    def basic_infection_heuristic(mem_props):
+    def infection_heuristic(mem_props):
         age = int(mem_props["age"])
         if 0 <= age <= 4:
-            return 0.0330660325674 / 6
+            return 0.0330660325674 / 8
         elif 5 <= age <= 14:
-            return 0.148281618844 / 6
+            return 0.148281618844 / 8
         elif 15 <= age <= 34:
-            return 0.304042732216 / 6
+            return 0.304042732216 / 8
         elif 35 <= age <= 59:
-            return 0.359434902279 / 6
+            return 0.359434902279 / 8
         elif 60 <= age <= 79:
-            return 0.109905306538 / 6
+            return 0.109905306538 / 8
         else:
-            return 0.0452694075548 / 6
+            return 0.0452694075548 / 8
 
-    def basic_mortality_heuristic(mem_props):
+    def mortality_heuristic(mem_props):
         age = int(mem_props["age"])
         if 0 <= age <= 4:
-            return 0.000059062155147 / 6
+            return 0.000059062155147 / 14
         elif 5 <= age <= 14:
-            return 0.0000124773674418 / 6
+            return 0.0000124773674418 / 14
         elif 15 <= age <= 34:
-            return 0.000111900272854 / 6
+            return 0.000111900272854 / 14
         elif 35 <= age <= 59:
-            return 0.00176127729351 / 6
+            return 0.00176127729351 / 14
         elif 60 <= age <= 79:
-            return 0.0338030065822 / 6
+            return 0.0338030065822 / 14
         else:
-            return 0.170387357522 / 6
+            return 0.170387357522 / 14
 
     simulation_settings = {
         "population_file": "DE_03_KLLand.csv",
-        "infection_probability_heuristic": basic_infection_heuristic,
-        "mortality_probability_heuristic": basic_mortality_heuristic,
+        "infection_probability_heuristic": infection_heuristic,
+        "mortality_probability_heuristic": mortality_heuristic,
         "number_of_initially_infected": 10,
         "number_of_initially_recovered": 0,
         "number_of_initially_vaccinated": 0,
@@ -52,16 +52,17 @@ if __name__ == "__main__":
         "recovered_immunity_time": 90,
         "vaccine_available_as_of": 365,
         "vaccination_takes_effect_time": 14,
-        "vaccinations_per_day": 720,
+        "vaccinations_per_day": 360,
         "vaccination_immunity_time": 90,
+        "vaccination_reliability": 0.9,  # FIXME make vaccinated infectable
         "waiting_time_vaccination_until_new_vaccination": 90,
         "waiting_time_recovered_until_vaccination": 90,
         "tests_per_day": 1000,
         "test_vaccinated": True,  # FIXME make vaccinated infectable
         "quarantine_duration": 10,
         "maximal_simulation_time_interval": 2*365,
-        "start_lockdown_at": 200,
-        "end_lockdown_at": 50
+        "start_lockdown_at": 150,
+        "end_lockdown_at": 20
     }
 
     # values = np.zeros(5)
@@ -78,8 +79,9 @@ if __name__ == "__main__":
     # T4 = threading.Thread(target=multi_sim, args=(4,)).start()
 
     # print(values)
-    # Scenarios.single_simulation(simulation_settings)
     # Scenarios.mitigation_interval(simulation_settings, (1.5, 3), 16, 1)
+
+    Scenarios.single_simulation(simulation_settings)
 
     sim_path = "../out/DE_03_KLLand/0004"
     PostProcessing.progression_plots(sim_path)
