8397750d4bdc2126a952fea2e99379468455b2a9
prokosch <prokosch@rhrk.uni-kl.de>
2022-02-08 16:42:36 +0100

    Reimplemented safety assertion

--- docs/makedocs.py
+++ docs/makedocs.py
@@ -4,8 +4,7 @@ from os.path import sep
 if __name__ == "__main__":
     os.system("pdoc3 --html --config latex_math=True --output-dir . .." + sep + "src")
 
-    assert(os.getcwd().split(sep)[-1] == "docs")
-    # TODO always navigate in docs no matter the cwd and do CI
+    assert os.getcwd().split(sep)[-1] == "docs"
 
     for file in os.listdir("src"):
         os.rename(os.getcwd() + sep + "src" + sep + file, os.getcwd() + sep + file)
